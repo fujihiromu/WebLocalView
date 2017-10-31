df9885f635139431e70946e0aba7738295f2bf45
HiromuFuji <fuji.hiromu@gmail.com>
2017-10-31 10:25:06 +0900

    Initial Commit

---
+++ WebViewLocalExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebViewLocalExample
+//
+//  Created by Fuji Hiromu on 2017/10/31.
+//  Copyright © 2017 藤　大夢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
