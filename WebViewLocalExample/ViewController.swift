aefb7cd508ad749476ae55ba06aad0bc779e92d1
HiromuFuji <fuji.hiromu@gmail.com>
2017-11-12 14:48:51 +0900

    first commit

--- WebViewLocalExample/ViewController.swift
+++ WebViewLocalExample/ViewController.swift
@@ -1,25 +1,40 @@
-//
-//  ViewController.swift
-//  WebViewLocalExample
-//
-//  Created by Fuji Hiromu on 2017/10/31.
-//  Copyright © 2017 藤　大夢. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIWebViewDelegate {
+    
+    var webView: UIWebView = UIWebView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // デリゲートを指定する
+        self.webView.delegate = self;
+        
+//        // 全画面表示にする
+//        self.webView.frame = self.view.bounds
+        
+        // サブビューを追加する
+        self.view.addSubview(self.webView)
+        
+        // index.htmlのパスを取得する
+        let path = Bundle.main.path(forResource: "index", ofType: "html")!
+        let url = NSURL(string: path)!
+        
+        // リクエストを生成する
+        let urlRequest = NSURLRequest(url : url as URL)
+        
+        // 指定したページを読み込む
+        self.webView.loadRequest(urlRequest as URLRequest)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func btnWebView(_ sender: Any) {
+        // 全画面表示にする
+        self.webView.frame = self.view.bounds
+    }
 }
-
